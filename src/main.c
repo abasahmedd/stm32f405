862192de1c1e0eeb2811f4022118f28cefa02684
Abbas Ogal <132055793+abasahmedd@users.noreply.github.com>
2025-05-23 13:32:19 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,19 +2,19 @@
 #include "stm32f1xx_hal.h"
 #include <string.h>
 
-extern UART_HandleTypeDef huart2;  // لازم تكون مفعل USART2 من CubeMX
+extern UART_HandleTypeDef huart2;  
 
 int main(void)
 {
   HAL_Init();
-  SystemClock_Config(); // مهيئة تلقائي من STM32Cube
-  MX_USART2_UART_Init(); // دالة تهيئة USART2 من STM32Cube
+  SystemClock_Config(); 
+  MX_USART2_UART_Init(); 
 
   char msg[] = "STM32 is alive!\r\n";
 
   while (1)
   {
     HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
-    HAL_Delay(1000); // تأخير ثانية
+    HAL_Delay(1000); 
   }
 }
